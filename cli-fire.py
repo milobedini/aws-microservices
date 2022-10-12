2cfa2a3aa912baec18b254dc92a516946a01076d
Milo Bedini <milobedini64@gmail.com>
2022-10-12 15:35:02 +0100

    added new cli functionality

--- cli-fire.py
+++ cli-fire.py
@@ -4,10 +4,18 @@
 
 import fire
 
-from mylib.logic import wiki
+from mylib import logic
 
 # Only run code if run as a script:
 if __name__ == "__main__":
-    fire.Fire(wiki)
+    fire.Fire(logic)
 
 # Takes flags such as name and length: ./cli-fire.py --name="Napoleon" --length=10
+
+""" 
+We have now allowed it to take in the whole logic module so it can search or 
+look up a particular person.
+e.g. 
+./cli-fire.py search_wiki "Barrack"
+./cli-fire.py wiki "Napoleon"
+ """
